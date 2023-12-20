40f1a88f41cdf8f6a4fff337fcf463a5e78bfffa
antho <2r4u666@gmail.com>
2023-12-19 17:38:10 -0800

    removed uneccessary led section

--- public/js/index.js
+++ public/js/index.js
@@ -4,11 +4,8 @@ socket.on('updateClient', data => {
     var lamp = document.getElementById("lamp");
     var led = document.getElementById("led");
 
-    (data.lamp  === 0 ? (lamp.innerHTML = "Lamp Off", lamp.style.color = "#fff") :
-        (lamp.innerHTML = "Lamp On", lamp.style.color = "#ffff00"));
-
-    (data.led === 0 ? (led.innerHTML = "LED Off", led.style.color = "#fff") :
-        (led.innerHTML = "LED On", led.style.color = "#ffff00"));
+    (data.lamp  === 0 ? (lamp.innerHTML = "Lights Off", lamp.style.color = "#fff") :
+        (lamp.innerHTML = "Lights On", lamp.style.color = "#ffff00")); 
 });
 
 function toggle(pin) {
